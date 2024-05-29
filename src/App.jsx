85e4b463335fc89756a61257e86ff3df12329a64
viji <vijayabharanikhaamahalingam@gmail.com>
2024-05-29 12:13:36 +0530

    link

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import Register from "./assets/pages/Register";
+import Login from "./assets/pages/Login";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// create a router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children:[
+      {
+        path:"/",
+        element :<div>
+           <Link to="/register">Register</Link>&nbsp;|&nbsp;  <Link to="/login">Login</Link>
+        </div>
+      },
+      {
+        path: "/register",
+        element: <Register />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      }
+    ]
+  }
+ 
+]);
+const App = () => {
+  // wrap the app with the RouterProvider
+  return <RouterProvider router={router} />;
 }
-
-export default App
+export default App;
---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+const Home = () => {
+    return (
+      <div>
+        <h1>Welcome to our Website</h1>
+        <p>Please register or login to access our services</p>
+       
+        <Outlet/>
+      </div>
+    )
+  }
+  
+  export default Home;
